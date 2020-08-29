d4c7864340a7f0a62f23ff120c22daf811771bca
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-08-29 12:07:23 +0200

    Create audioBookB.py

---
+++ audioBookB.py
@@ -0,0 +1,93 @@
+import pyttsx3
+import gtts
+from tkinter import *
+from tkinter import filedialog
+import tkinter.scrolledtext as scrolledtext
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.layout import LAParams
+from pdfminer.converter import TextConverter
+from io import StringIO
+from pdfminer.pdfpage import PDFPage
+from googletrans import Translator
+import threading
+
+class App:
+    def __init__(self):
+
+        self.ventana = Tk()
+        self.ventana.configure(bg='dim gray')
+        self.ventana.geometry("801x490")
+        self.ventana.title("LECTOR PDFs")
+        self.rate=IntVar()
+        self.rate.set(130)
+        self.translator = Translator()
+        self.text = ""
+        
+        self.resource_manager = PDFResourceManager(caching=True)
+        
+        self.btnSearch = Button(self.ventana,text="BUSCA PDF",command=self.init_open)
+        self.btnSearch.place(x=1,y=7)
+        self.btnListen = Button(self.ventana,text="LEER",command=self.initRead)
+        self.btnListen.place(x=90,y=7)
+        self.display=scrolledtext.ScrolledText(self.ventana,background='white',width=97,height=28)
+        self.display.pack(side='bottom')
+        self.player = pyttsx3.init()
+        self.label = Label(self.ventana,text='Speech Rate:',bg='dim gray',fg='white')
+        self.label.place(x=150,y=9)
+        self.entry = Entry(self.ventana,width=6,textvariable=self.rate)
+        self.entry.place(x=227,y=9)
+
+        self.ventana.mainloop()
+
+    def open(self):
+        file = filedialog.askopenfilename(initialdir="/",title="SELECCIONAR ARCHIVO",
+                    filetypes =(("PDF files","*.pdf") ,("all files","*.*")))
+        if file != "":
+            self.display.delete('1.0',END)
+            #resource_manager = PDFResourceManager(caching=True)
+            out_text = StringIO()
+            codec = 'utf-8'
+            laParams = LAParams()
+            text_converter = TextConverter(self.resource_manager, out_text, laparams=laParams)
+            fp = open(file, 'rb')
+            interpreter = PDFPageInterpreter(self.resource_manager, text_converter)
+            for page in PDFPage.get_pages(fp, pagenos=set(), maxpages=0, password="", caching=True, check_extractable=True):
+                interpreter.process_page(page)
+
+            self.text = out_text.getvalue()
+
+            fp.close()
+            text_converter.close()
+            out_text.close()
+
+            self.display.insert(END,self.text)
+            self.detect_lang()
+
+    def detect_lang(self):
+        if len(self.display.get('1.0',END)) > 1:
+            try:
+                print(type(self.text))
+                self.lang = (self.translator.translate(self.text).src)
+                print(self.lang)
+            except:
+                pass
+            
+
+    def read_text(self):
+        self.player.setProperty('rate',int(self.entry.get()))
+            
+        self.player.say(self.text)
+        self.player.runAndWait()
+
+    def initRead(self):
+        t = threading.Thread(target=self.read_text)
+        t.start()
+
+    def init_open(self):
+        t0 = threading.Thread(target=self.open)
+        t0.start()
+            
+
+
+if __name__=="__main__":
+    App()
