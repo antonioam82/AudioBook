b9585580ada6adc04ec5adbae59995a5216dcee5
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-09-29 16:11:10 +0200

    Update audioBook.py

--- audioBook.py
+++ audioBook.py
@@ -21,29 +21,33 @@ class App:
         self.ventana.geometry("801x511")
         self.ventana.title("LECTOR DE TEXTOS")
         self.rate=IntVar()
+        self.current_dir = StringVar()
+        self.current_dir.set(os.getcwd())
         self.rate.set(130)
         self.text = ""
         self.actv = False
         
         self.resource_manager = PDFResourceManager(caching=True)
         
+        self.entry = Entry(self.ventana,textvariable=self.current_dir,bg='light gray',width=133)
+        self.entry.pack(side=TOP)
         self.btnSearch = Button(self.ventana,text="BUSCA PDF",command=lambda:self.init_task(1))
-        self.btnSearch.place(x=1,y=7)
+        self.btnSearch.place(x=1,y=25)
         self.btnListen = Button(self.ventana,text="LEER",command=lambda:self.init_task(0))
-        self.btnListen.place(x=90,y=7)
+        self.btnListen.place(x=90,y=25)
         self.label2 = Label(self.ventana,bg='dim gray',fg='white')
         self.label2.pack(side='bottom')        
-        self.display=scrolledtext.ScrolledText(self.ventana,background='white',width=97,height=28)
+        self.display=scrolledtext.ScrolledText(self.ventana,background='white',width=97,height=27)
         self.display.pack(side='bottom')
         self.player = pyttsx3.init()
         self.label = Label(self.ventana,text='Speech Rate:',bg='dim gray',fg='white')
-        self.label.place(x=150,y=9)
+        self.label.place(x=150,y=30)
         self.entry = Entry(self.ventana,width=6,textvariable=self.rate)
-        self.entry.place(x=227,y=9)
+        self.entry.place(x=227,y=30)
         self.btnSave = Button(self.ventana,text='GUARDA AUDIO',command=lambda:self.init_task(2))
-        self.btnSave.place(x=300,y=7)
+        self.btnSave.place(x=300,y=25)
         self.btnDir = Button(self.ventana,text='SELECT FOLDER',command=self.change_dir)
-        self.btnDir.place(x=700,y=7)
+        self.btnDir.place(x=700,y=25)
 
         self.ventana.mainloop()
 
@@ -51,6 +55,7 @@ class App:
         direct = filedialog.askdirectory()
         if direct != "":
             os.chdir(direct)
+            self.current_dir.set(os.getcwd())
         
 
     def open(self):
