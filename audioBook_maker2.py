e37827e0ab83a3bea002727a6ede4af8491cf7b8
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2022-05-11 18:47:02 +0200

    Update audioBook_maker2.py

--- audioBook_maker2.py
+++ audioBook_maker2.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import pyttsx3
 from tkinter import *
-from tkinter import filedialog, messagebox
+from tkinter import filedialog, messagebox, ttk
 import tkinter.scrolledtext as scrolledtext
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.layout import LAParams
@@ -18,7 +18,7 @@ class App:
 
         self.ventana = Tk()
         self.ventana.configure(bg='dim gray')
-        self.ventana.geometry("1061x600")#1000 n630
+        self.ventana.geometry("1061x620")#1000 n630
         self.ventana.title("AUDIO-TEXT MAKER")
         self.rate=IntVar()
         self.current_dir = StringVar()
@@ -30,13 +30,17 @@ class App:
         self.resource_manager = PDFResourceManager(caching=True)
         
         Entry(self.ventana,textvariable=self.current_dir,bg='light gray',width=176).pack(side=TOP)
-        Button(self.ventana,text="SEARCH PDF",command=self.open_file).place(x=9,y=28)
+        Button(self.ventana,text="SEARCH PDF",command=self.init_task).place(x=9,y=28)
+        #Button(self.ventana,text="<").pack(side='bottom')
+        #Button(self.ventana,text=">").pack(side='right')
         #self.btnListen = Button(self.ventana,text="LEER")
         #self.btnListen.place(x=90,y=25)
+        self.pageList = ttk.Combobox(self.ventana,width=12)
+        self.pageList.place(x=960,y=29)
         self.label2 = Label(self.ventana,bg='dim gray',fg='white')
         self.label2.pack(side='bottom')        
-        self.display=scrolledtext.ScrolledText(self.ventana,background='white',width=128,height=32)#width=120,height=32
-        self.display.pack(side='bottom')
+        self.display=scrolledtext.ScrolledText(self.ventana,background='white',width=128,height=33)#width=120,height=32
+        self.display.place(x=9,y=62)
         self.player = pyttsx3.init()
         #self.label = Label(self.ventana,text='Speech Rate:',bg='dim gray',fg='white')
         #self.label.place(x=150,y=30)
@@ -53,7 +57,7 @@ class App:
         pdf_file = filedialog.askopenfilename(initialdir="/",title="SELECT FILE",
                         filetypes=(("PDF files","*.pdf"),("all files","*.*")))
         if pdf_file:
-            pages = 0
+            self.pages = 0
             #self.name,ex = os.path.splitext((pdf_file.split('/')[-1]))
             self.name = pdf_file.split('/')[-1]
             out_text = StringIO()
@@ -63,14 +67,27 @@ class App:
             interpreter = PDFPageInterpreter(self.resource_manager, text_converter)
             with open(pdf_file, 'rb') as fp:
                 for page in PDFPage.get_pages(fp, pagenos=set(), maxpages=0, password="", caching=True, check_extractable=True):
-                    #if pages == 3-1:  #pagina deseada - 1
+                    #if pages == 12-1:  #pagina deseada - 1
                     interpreter.process_page(page)
-                    pages += 1
-                
+                    self.pages += 1
+                    
+            self.n_pages()      
             self.text = out_text.getvalue()
             self.display.insert(END, self.text)
+            self.text = ""
             
-            self.label2.configure(text="TITTLE: {} (PAGES: {})".format(self.name,pages))
+            self.label2.configure(text="TITTLE: {} (PAGES: {})".format(self.name,self.pages))
+
+    def init_task(self):
+        t = threading.Thread(target=self.open_file)
+        t.start()
+
+    def n_pages(self):
+        list_of_pages = []
+        for i in range(self.pages):
+            list_of_pages.append("PAGE {}".format(i+1))
+        self.pageList["values"] = list_of_pages
+        self.pageList.set("ALL")
 
             
 if __name__=="__main__":
