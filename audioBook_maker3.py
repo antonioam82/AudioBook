1c4474afe3061299b6462bd39df77b40b09c0537
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2022-05-21 16:00:23 +0200

    Update audioBook_maker3.py

--- audioBook_maker3.py
+++ audioBook_maker3.py
@@ -11,7 +11,7 @@ import os
 class App:
     def __init__(self):
         self.root = Tk()
-        self.root.geometry("803x300")
+        self.root.geometry("837x300")
         self.root.title("AUDIOBOOK MAKER")
 
         current_dir = StringVar()
@@ -19,8 +19,8 @@ class App:
         self.file_name = StringVar()
 
         Entry(self.root,textvariable=current_dir,width=133).place(x=0,y=0)
-        Button(self.root,text="SEARCH",height=2,width=10).place(x=10,y=30)
-        Entry(self.root,textvariable=self.file_name,width=33,font=('arial',24)).place(x=96,y=30)
+        Button(self.root,text="SEARCH",height=2,width=10).place(x=10,y=44)
+        Entry(self.root,textvariable=self.file_name,width=33,font=('arial',24)).place(x=96,y=44)
 
         self.root.mainloop()
 
