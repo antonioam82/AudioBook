b2cf06a9ee7d1aa53370c72062203daf7c6d4911
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2022-05-21 15:54:48 +0200

    Update audioBook_maker3.py

--- audioBook_maker3.py
+++ audioBook_maker3.py
@@ -16,10 +16,14 @@ class App:
 
         current_dir = StringVar()
         current_dir.set(os.getcwd())
+        self.file_name = StringVar()
 
         Entry(self.root,textvariable=current_dir,width=133).place(x=0,y=0)
+        Button(self.root,text="SEARCH",height=2,width=10).place(x=10,y=30)
+        Entry(self.root,textvariable=self.file_name,width=33,font=('arial',24)).place(x=96,y=30)
 
         self.root.mainloop()
 
 if __name__ == "__main__":
     App()
+        
