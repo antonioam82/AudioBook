907a48910422c976a9cbc0d02f83cd9f564fdf8a
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2022-05-21 15:45:00 +0200

    Update audioBook_maker3.py

--- audioBook_maker3.py
+++ audioBook_maker3.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 import pyttsx3
 import PyPDF2
-from tkinter import filedialog, messagebox, ttk, Tk
-
+from tkinter import *
+from tkinter import filedialog, messagebox, ttk
 from googletrans import Translator
 import threading
 import os
@@ -11,8 +11,13 @@ import os
 class App:
     def __init__(self):
         self.root = Tk()
-        self.root.geometry("800x300")
+        self.root.geometry("803x300")
         self.root.title("AUDIOBOOK MAKER")
+
+        current_dir = StringVar()
+        current_dir.set(os.getcwd())
+
+        Entry(self.root,textvariable=current_dir,width=133).place(x=0,y=0)
 
         self.root.mainloop()
 
